1087c52661c51a6cacb646513537ae904a2e8232
Lyly <hadongit66@gmail.com>
2022-01-09 15:46:21 +0700

    Navigation between Pages
    How to prefetch working?
    - Only prefetch links that appear in the viewport
    - It use intersection Observer API to detect
    - Prefetching is only anable in production
    -But it will be disable in slow network or when user have Save-Data turn on

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,22 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  const router = useRouter()
+  function goToPostDetail() {
+    router.push({
+      pathname: 'posts/[postId]',
+      query: {
+        postId: '123',
+        ref: 'social',
+      },
+    })
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -20,6 +33,10 @@ const Home: NextPage = () => {
         <p className={styles.description}>
           Get started by editing <code className={styles.code}>pages/index.tsx</code>
         </p>
+        <Link href="/about">
+          <a>Go to about</a>
+        </Link>
+        <button onClick={goToPostDetail}>Go to Post detail</button>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
